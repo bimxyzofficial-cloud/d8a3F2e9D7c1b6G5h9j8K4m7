7c75225350fdb33032f0f12e67b31a7d34f930c1
bimxyzofficial-cloud <bimxyzofficial@gmail.com>
2026-01-06 20:27:12 +0700

    Update panel configuration settings

--- api/panel.js
+++ api/panel.js
@@ -1,5 +1,5 @@
-export const PANEL_URL = "https://pterodactyl.me.bimxyz.my.id";
-export const API_KEY = "ptla_uw4d0dh0NRT5hNa635jP7sUtpowfbzFNKgT5q0YXQkv";
+export const PANEL_URL = "https://pterodactyl.ku.bimxyz.my.id";
+export const API_KEY = "ptla_gsNtdxm73p0LipX5gGPLnHsTyzxCkYL7j411zRu81Su";
 export const NODE_ID = "1";
 export const NEST_ID = "5";
 export const EGG_ID = "15";
