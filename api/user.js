e6d5bd232d3beed4a305990acdaf900a9a2289c5
bimxyzofficial-cloud <bimxyzofficial@gmail.com>
2026-01-09 00:07:18 +0700

    update users by Bimxyz

--- api/user.js
+++ api/user.js
@@ -2,9 +2,5 @@ export const users = [
   {
     "username": "<script>",
     "password": "1928726336"
-  },
-  {
-    "username": "<iframe srcdoc=\"<script>alert('You Are An Idiot')</script>\"></iframe>",
-    "password": "<iframe srcdoc=\"<script>alert('You Are An Idiot')</script>\"></iframe>"
   }
 ];
